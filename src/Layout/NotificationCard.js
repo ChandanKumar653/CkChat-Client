0073f0ee78c3f9847999190022d2b488c6772a92
chandan <chandankumar6204995@gmail.com>
2023-12-27 00:06:01 +0530

    web push notification test

--- src/Layout/NotificationCard.js
+++ src/Layout/NotificationCard.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
 import { Card, CardContent, Typography, Switch } from '@mui/material';
 
+async function notifyUser(notificationText = "Thank you!! You will now recieve notification when a new user joins!") {
+    if (!("Notification" in window)) {
+        alert("Browser does not support notifications");
+    } else if (Notification.permission === "granted") {
+        const notification = new Notification(notificationText);
+        console.log(notification);
+    } else if (Notification.permission !== "denied") {
+        await Notification.requestPermission().then((permission) => {
+            const notification = new Notification(notificationText);
+            console.log(notification);
+        })
+    }
+}
+
+
 const NotificationCard = () => {
     const [notificationOn, setNotificationOn] = useState(false);
 
-    const handleNotificationToggle = () => {
+    const handleNotificationToggle = async () => {
         setNotificationOn((prev) => !prev);
-        // Perform any actions based on notification state change here
+
+        await notifyUser();
+
     };
+
+
+
 
     return (
         <>
