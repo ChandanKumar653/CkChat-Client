ac1836314ffadd6b173b82947f6616b4a5a31d69
chandan <chandankumar6204995@gmail.com>
2023-12-26 21:45:26 +0530

    updated git

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from 'react';
 import Layout from './Layout'
 export default function App() {
@@ -34,4 +7,3 @@ export default function App() {
     </>
   )
 }
->>>>>>> a967d19 (updated site with whatsapp share)
